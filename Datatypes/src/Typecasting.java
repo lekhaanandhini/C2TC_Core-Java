5a0bf8986355e4190b4f6f676ef4cbc755926b4b
admin <admin@LAPTOP-BJ6AJREU>
2022-03-05 09:54:01 +0530

    first commit

---
+++ Datatypes/src/Typecasting.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Demo { 
+public void show() 
+	{ 
+   ArrayList<String> list = new ArrayList<String>(); 
+		System.out.print(list.get(0)); 
+	} 
+} 
+public class Typecasting{
+	    public static void main(String[] args) { 
+			Demo demo = new Demo(); 
+			demo.show(); 
+
+	}
+
+}
