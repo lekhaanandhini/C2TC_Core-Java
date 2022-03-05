58d77ce45bfdba59549d28c84966c75b141b73ac
admin <admin@LAPTOP-BJ6AJREU>
2022-03-05 09:57:33 +0530

    first commit

---
+++ Interface/src/example.java
@@ -0,0 +1,17 @@
+class dog implements Check{
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
+public class example{
+   public static void main(String args[])
+   {
+	  dog obj = new dog();
+	   System.out.println(obj instanceof animal);
+   } }
