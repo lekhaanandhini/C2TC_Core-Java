06e1f003a7fb348c4789b154a186aab42f0176c9
admin <admin@LAPTOP-BJ6AJREU>
2022-03-05 09:57:09 +0530

    first commit

---
+++ Inheritance/src/multiple.java
@@ -0,0 +1,18 @@
+class Animal{
+	void eat() {
+		System.out.println("eating");
+	}
+}
+class Dog{
+	void bark() {
+		System.out.println("eating meat");
+	}
+}
+public class multiple extends Animal,Dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Inheritance/src/single.java
@@ -0,0 +1,16 @@
+class A{
+	String color="white";
+}
+class B extends A{
+	String color="black";
+	void printcolor() {
+		System.out.println(color);
+	}
+}
+class single{
+public static void main(String args[])
+    {
+   Dog obj=new Dog();
+    obj.printcolor();
+     }
+}
